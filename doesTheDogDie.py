b1d63bac6224ac212911b97d71ddfe264c9c76b0
BritishCopsAreClowns <finnryanit@gmail.com>
2023-12-25 14:52:22 +0000

    Content warnings work fine for movies. Would like to add a better system for cleaning up what the tags are that are causing the warning.

--- doesTheDogDie.py
+++ doesTheDogDie.py
@@ -4,6 +4,7 @@ import json
 import os
 import emoji
 import sqlite3
+import re
 # Will need DB for the following
 ## Media Libraries
 ### ID / Library Name / Library Type / DTDD Relevant tag
@@ -11,26 +12,39 @@ import sqlite3
 ### Media Name / Media ID / DTDD ID / Media Type ID 
 
 DTDD_QUERY_URL = 'https://www.doesthedogdie.com/dddsearch?q='
+DTDD_ID_URL = 'https://www.doesthedogdie.com/media/'
 DTDD_KEY = os.getenv('DTDD_KEY')
 PLEX_URL = os.getenv('PLEX_URL') + ":32400/"
 PLEX_KEY = os.getenv('PLEX_KEY')
 
 # TODO #1 Move me to a place where I can be changed easily
 excludedLibraries = [10,13,14]
-triggerIDList = [182,201,292,299,320,326]
+triggerIDWarn = [201,326]
+TriggerIDAlert = [182,292]
+triggerIDList = triggerIDWarn + TriggerIDAlert
 
 
 """ Trigger List (clean up later) - I DID NOT RANK THESE THIS IS THE TOPIC IDS FROM DTDD
 TopicId = Topic Name < Key
-    182 = Sexual Assualt (Onscreen?)
-    201 = Vomitting
-    292 = Rape Onscreen
-    299 = Drugged
-    320 = Pedo
-    326 = Rape Mentioned
+    182 = Sexual Assualt (Onscreen?) / Alert
+    201 = Vomitting / Warn
+    292 = Rape Onscreen / Alert
+    299 = Drugged / Warn
+    320 = Pedo / Warn
+    326 = Rape Mentioned / Warn
 """
 
 #TODO #2 Add TMDB API to check more information
+def dtddLookup(ID):
+    headers = {
+    'Accept': 'application/json',
+    'X-API-KEY': DTDD_KEY
+    }
+    response = requests.get(DTDD_ID_URL + str(ID) ,headers = headers)
+    if response.status_code == 200 and len(json.loads(response.content)['item']) > 0:
+        return json.loads(response.content)
+    else:
+        return f'Failed to find item'
 
 
 def queryDTDD(query):
@@ -63,30 +77,113 @@ def mediaCheck(plexitem):
     dtddResults = queryDTDD(plexitem['title'])
     if dtddResults == 'No results': # Pass dtdd response down
         return dtddResults
-    # Compare items, create confidence score
-    for result in dtddResults['items']:
-        
+
+    def updatePlexItem(mediaDetails,TriggerIDs):
+        def triggerString(IDs):
+            eWarn = warn = alert = False
+            tString = ''
+            for ID in IDs:
+                if int(ID) in triggerIDWarn:
+                    if int(ID) == 201:
+                        eWarn = True # Extra warning for single user 
+                    else:
+                        warn = True
+                if int(ID) in TriggerIDAlert:
+                    alert = True
+            if alert == True:
+                tString += f':cross_mark: - M '
+            elif warn == True:
+                tString += f':warning: - M '
+            if eWarn == True:
+                tString += f':warning: - E'
+            if alert == False and warn == False and eWarn == False:
+                tString = f':check_mark:'
+            return emoji.emojize(tString)
+        if TriggerIDs == 'Unknown':
+            ValueString = emoji.emojize('? NO DATA ?')
+        elif TriggerIDs == 'Safe':
+            ValueString =  emoji.emojize(':check_mark:')
+        else:
+            ValueString = triggerString(TriggerIDs)
+        request = f'library/sections/{mediaDetails["libraryID"]}/all?type=1&id={mediaDetails["itemID"]}&includeExternalMedia=1&contentRating.value={ValueString}'
+        attemptUpdate = requests.put(PLEX_URL + request, headers=headers)
+        print(attemptUpdate.status_code)
+
+    def mediaMatch(mediaItem,searchResults):
         confidence = 0
-        if result['name'].lower() == plexitem['title'].lower():
-            confidence +=20
-        try:
-          if result['genre'].lower() in plexitem['Genre'].lower():
-              confidence +=25
-        except:
-            confidence +=0
-        try: # Sometimes DTDD will return 'unknown' for the year. #TODO #3 Add better failure prevention to the confidence checker
-          confidence += 25 if int(result['releaseYear']) == int(plexitem['year']) else 20 if int(result['releaseYear']) == int(plexitem['year'])+1 or int(result['releaseYear']) == int(plexitem['year'])-1 else 0
-        except:
-            confidence +=0
-        if confidence > 40 and len(result['stats']) > 4:
-            resultDict = json.loads(result['stats'])['topics']
-            for triggerID, triggerValue in resultDict.items():
-                if int(triggerID) in triggerIDList:
-                    if triggerValue["definitelyYes"] >= 1:  
-                      print(f'result for {plexitem["title"]}(confidence {confidence})\nTrigger ID: {triggerID}\n- - Not present: {triggerValue["definitelyNo"]}\n- - Present: {triggerValue["definitelyYes"]}')
-                
+        # Creat confidence score for results.
+        for result in searchResults['items']:
+            confidence = 0
+            while confidence < 100:
+                if mediaItem['mediaTypeTrue'] != 'show':
+                    if str(result['tmdbId']) == str(mediaItem['dbIDs']['tmdb']):
+                        confidence += 100
+                        break
+                else:
+                    try:
+                        if str(result['tmdbId']) == str(mediaItem['dbIDs']['tmdb']):
+                            confidence += 100
+                            break
+                    except:
+                        pass
+                if str(result['imdbId']) == str(mediaItem['dbIDs']['imdb']):
+                    confidence += 100
+                    break
+                if result['name'].lower() == mediaItem['title'].lower():
+                    confidence += 20
+                # if result['genre'].lower() in mediaItem['Genre'].lower():
+                #     confidence += 25
+                if result['releaseYear'].isnumeric():
+                    if int(result['releaseYear']) == int(plexitem['releaseYear']):
+                        confidence += 25
+                    # Sometimes DTDD will return 'unknown' for the year. #TODO #3 Add better failure prevention to the confidence checker
+                    elif int(result['releaseYear']) == int(plexitem['releaseYear'])+1 or int(result['releaseYear']) == int(plexitem['releaseYear'])-1:
+                        confidence += 20
+                break
+            if confidence >= 100:
+                return result
+            # Failed to find match
+        return 'No results'
+        
 
 
+    
+    result = mediaMatch(plexitem,dtddResults)
+    try:
+        resultDict = json.loads(result['stats'])['topics']
+    except:
+        resultDict = False
+    if result != 'No results' and resultDict !=False:
+        safe = 0
+        conflict = 0
+        triggersPresent = []
+        detailedResult = dtddLookup(result['id'])
+        for groups in detailedResult['allGroups']:
+            for items in groups['topics']:
+                if items['TopicId'] in triggerIDList:
+                    yesSum = items['yesSum']
+                    noSum = items['noSum']
+                    if yesSum == 0 and noSum == 0: # Unsure
+                        print('No data')
+                    elif (yesSum > noSum and noSum == 0) or (yesSum > noSum and yesSum/noSum > 1.5): # Trigger is present
+                        triggersPresent.append(items['TopicId'])
+                    elif (yesSum == 0 and noSum > 0) or (noSum > yesSum) or noSum/yesSum > 1.5: #Tigger not present
+                        safe += 1 
+                    elif noSum/yesSum > 1 and noSum/yesSum < 1.5: # Conflicting response
+                        conflict += 1
+                    else:
+                        print('unknown')
+                    print(items['comment'])
+        if len(triggersPresent) > 0:
+            updatePlexItem(plexitem,triggersPresent)
+        elif result == 'No results':
+            updatePlexItem(plexitem,'Unknown')
+        elif len(triggersPresent) == 0:
+            updatePlexItem(plexitem,'Safe')
+    
+    else:
+        pass
+    #TODO ADD notice to plex showing failure to obtain value
 # database = sqlite3.connect('plex_dog.db')
 
 # Test query, remove
@@ -98,7 +195,7 @@ headers = {
   'Accept': 'application/json',
   'X-Plex-Token': PLEX_KEY
   }
-
+"""
 response = requests.get(PLEX_URL + 'library/metadata/2', headers=headers)
 movieDetails = json.loads(response.content)
 
@@ -109,7 +206,7 @@ updateValue = 102
 request = f'library/sections/{libID}/all?type=1&id={movieID}&includeExternalMedia=1&{updateFieldName}.value={updateValue}'
 attemptUpdate = requests.put(PLEX_URL + request, headers=headers)
 print(attemptUpdate)
-
+"""
 
 """ Fields
 RatingKey - Cannot be changed
@@ -139,38 +236,36 @@ artist - Audio media (podcasts, music, etc) Ignore
 # Get media list
 mediaList = {}
 for libID in libraryDetails: 
-    if str(libID) not in ['10','13','14','4']:
-        items = requests.get(PLEX_URL + 'library/sections/' + libID + '/all' ,headers=headers)
+    if str(libID) not in ['10','13','14','4','1']:
+        libraryItems = requests.get(PLEX_URL + 'library/sections/' + libID + '/all' ,headers=headers)
     else:
         print('skipping excluded library')
         continue
-    if items.status_code == 200:
-        itemJSON = json.loads(items.content)
+    if libraryItems.status_code == 200:
+        itemJSON = json.loads(libraryItems.content)['MediaContainer']['Metadata']
         # Check each item in media list
     else:
         print('failed to get library')
         continue
-    for item in itemJSON['MediaContainer']['Metadata']:
-        dtDDInfo = mediaCheck(item)
+    for itemID in itemJSON:
+        item = json.loads(requests.get(PLEX_URL + 'library/metadata/' + itemID['ratingKey'] ,headers=headers).content)['MediaContainer']['Metadata'][0]
+        itemGUIDs = item['Guid']
 
-        # Attempt to add IMDB Tag
-        try:
-            imDB = item['Guid']['0']['id'].replace('imdb://tt','')
-            tmDB = item['Guid']['1']['id'].replace('imdb://tt','')
-            idid = item['Guid']['2']['id'].replace('imdb://tt','')
-        except:
-            imDB = tmDB = 'no ID'
-        dtDDInfo = mediaCheck(item)
+        # Extract GUID Tags for matching later
+        guidDict = {}
+        trackID = ''
+        for ids in item['Guid']:
+            guidDict[ids['id'].split('://')[0]] = ids['id'].split('://')[1]
         mediaList[item['ratingKey']] = {
             'libraryID': libID,
+            'itemID': item['ratingKey'],
             'GUID': item['guid'],
             'title': item['title'],
+            'releaseYear': item['year'],
             'mediaTypeTrue': item['type'],
-            'imDB': imDB,
-            'triggerIDs':'dtDDInfo',
-            'confidence':'dtDDInfo'
-            
+            'dbIDs': guidDict,
         }
+        dtDDInfo = mediaCheck(mediaList[item['ratingKey']])
 
 for x in mediaList:
     print(x)
